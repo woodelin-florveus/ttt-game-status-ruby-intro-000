33cb6334ed6e229c49464b81906c02979a9bf57f
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-05 21:10:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,4 +63,15 @@ def over?(board)
 end
 
 def winner(board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
 end
