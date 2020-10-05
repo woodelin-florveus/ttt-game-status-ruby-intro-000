597e5379a5a66a9bf40ea3fbb2d423d2c3e29c9b
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-05 21:16:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,7 +32,7 @@ def won(board)
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        false
+        return win_combination
       end
   end
 
